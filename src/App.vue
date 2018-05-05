5e49445e2ab121e0a9a45fe7993ba35d3f6599bf
Victor Alves <vsilva.programador@gmail.com>
2018-05-05 13:41:57 -0300

    Minor updates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="d-flex flex-column">
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /*text-align: center;
+  color: #2c3e50;*/
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
 #nav {
   padding: 30px;
@@ -25,5 +30,9 @@
       color: #42b983;
     }
   }
+}
+
+.flex-1 {
+  flex: 1;
 }
 </style>
