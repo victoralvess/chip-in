cfcd0475cad1b1a5d2a1472711d90bb2168368dc
Victor Alves <vsilva.programador@gmail.com>
2018-05-05 13:40:17 -0300

    Set up atomic design approach

---
+++ src/components/Input.vue
@@ -0,0 +1,9 @@
+<template>
+  <input class="form-control">
+</template>
+
+<script>
+export default {
+  name: 'Input'
+}
+</script>
---
+++ src/components/Label.vue
@@ -0,0 +1,15 @@
+<template>
+  <label>{{ label }}</label>
+</template>
+
+<script>
+export default {
+  name: 'Label',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
