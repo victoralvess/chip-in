f9d8f4901af9e7f5232802080d6e69fb8f7a968a
Victor Alves <vsilva.programador@gmail.com>
2018-05-05 13:41:02 -0300

    Add sign in route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import SignIn from './views/SignIn.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/sign-in',
+      name: 'sign-in',
+      component: SignIn
     }
   ]
 })
