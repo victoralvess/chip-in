9979a72f0e84835dccb40d626e762918d77216e5
Victor Alves <vsilva.programador@gmail.com>
2018-05-06 14:33:24 -0300

    Add ProgressBar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar" :style="{ width: value + '%' }">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
