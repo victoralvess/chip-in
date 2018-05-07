a34685e226a8eb1dd0befb38d660d305de5c8e82
Victor Alves <vsilva.programador@gmail.com>
2018-05-07 17:56:27 -0300

    Remove NavBar of all pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <div id="app" class="d-flex flex-column">
-    <NavBar class="navbar-light bg-light">
-      <Button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="navbar-toggler-icon"></span>
-      </Button>
-      <NavBarNav>
-        <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
-        <router-link to="/about" class="nav-link">About</router-link>
-      </NavBarNav>
-    </NavBar>
     <router-view/>
   </div>
 </template>
@@ -41,15 +31,5 @@ a {
 
 <script>
 import 'bootstrap'
-import Button from '@/components/Button';
-import NavBar from '@/components/NavBar';
-import NavBarNav from '@/components/NavBarNav';
-
-export default {
-  components: {
-    Button,
-    NavBar,
-    NavBarNav
-  }
-}
+export default {}
 </script>
