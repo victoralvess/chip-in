2b13d2b43531d208fa098ca0d5a95a404a793320
Victor Alves <vsilva.programador@gmail.com>
2018-05-07 19:37:27 -0300

    Minor updates

--- src/components/compounds/NavigationBar.vue
+++ src/components/compounds/NavigationBar.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="container">
-    <NavBar class="navbar-light bg-light">
-      <Button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="navbar-toggler-icon"></span>
-      </Button>
-      <NavBarNav>
+  <NavBar class="navbar-light bg-light">
+    <Button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="navbar-toggler-icon"></span>
+    </Button>
+    <NavBarNav>
+      <div class="nav-item">
         <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+      </div>
+      <div class="nav-item dropdown">
+        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">Dashboard</a>
+        <div class="dropdown-menu">
+          <router-link to="/dashboard" class="dropdown-item">General View</router-link>
+          <router-link to="/dashboard/goals/create" class="dropdown-item">Create a Goal</router-link>
+        </div>
+      </div>
+      <div class="nav-item">
         <router-link to="/about" class="nav-link">About</router-link>
-      </NavBarNav>
-    </NavBar>
-  </div>
+      </div>
+    </NavBarNav>
+  </NavBar>
 </template>
 
 <script>
