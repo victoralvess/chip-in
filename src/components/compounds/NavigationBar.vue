2dbba1b420263f2964815573448e4b2e27d9465c
Victor Alves <vsilva.programador@gmail.com>
2018-05-07 17:55:55 -0300

    Add a separate Navigation Bar file

---
+++ src/components/compounds/NavigationBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <NavBar class="navbar-light bg-light">
+      <Button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="navbar-toggler-icon"></span>
+      </Button>
+      <NavBarNav>
+        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+        <router-link to="/about" class="nav-link">About</router-link>
+      </NavBarNav>
+    </NavBar>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/atoms/Button';
+import NavBar from '@/components/atoms/NavBar';
+import NavBarNav from '@/components/atoms/NavBarNav';
+
+export default {
+  name: 'NavigationBar',
+  components: {
+    Button,
+    NavBar,
+    NavBarNav
+  }
+}
+</script>
