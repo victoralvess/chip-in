a30212c12f1a8027cc2b3b5baeb5dab961a2d4dd
Victor Alves <vsilva.programador@gmail.com>
2018-05-08 22:52:52 -0300

    Make request at /authenticate

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,14 +2,14 @@
   <div class="flex-1 d-flex flex-column justify-content-center align-items-center">
     <Card>
       <CardBody>
-        <form method="post" action="/authenticate"  class="d-flex flex-column">
+        <form class="d-flex flex-column" @submit.prevent="signIn">
           <div class="form-group">
             <Label for="username" label="Username"/>
-            <Input type="text" id="username" name="username"/>
+            <Input type="text" id="username" name="username" v-model="username"/>
           </div>
           <div class="form-group">
             <Label for="password" label="Password"/>
-            <Input type="password" id="password" name="password"/>
+            <Input type="password" id="password" name="password" v-model="password"/>
           </div>
           <Button type="submit" class="btn-primary ml-auto mr-auto">Sign In</Button>
         </form>
@@ -25,6 +25,8 @@ import Label from '@/components/atoms/Label'
 import Input from '@/components/atoms/Input'
 import Button from '@/components/atoms/Button'
 
+import axios from 'axios';
+
 export default {
   name: 'sign-in',
   components: {
@@ -33,6 +35,29 @@ export default {
     Label,
     Input,
     Button
-  }  
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    signIn() {
+      const credentials = JSON.stringify({
+        username: this.username,
+        password: this.password
+      })
+      axios.post('/authenticate', credentials, {
+        headers: { 'Content-type': 'application/json' }
+      })
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
