146060151780db869fb9895badd1ba7a0f60ee5c
Victor Alves <vsilva.programador@gmail.com>
2018-05-09 16:26:46 -0300

    Minor updates

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,18 +2,18 @@
   <div class="flex-1 d-flex flex-column justify-content-center align-items-center">
     <Card>
       <CardBody>
-      <div class="alert alert-danger" v-if="error">{{error}}</div>
-        <form class="d-flex flex-column" @submit.prevent="signIn">
-          <div class="form-group">
-            <Label for="username" label="Username"/>
-            <Input type="text" id="username" name="username" v-model="username"/>
-          </div>
-          <div class="form-group">
-            <Label for="password" label="Password"/>
-            <Input type="password" id="password" name="password" v-model="password"/>
-          </div>
-          <Button type="submit" class="btn-primary ml-auto mr-auto">Sign In</Button>
-        </form>
+       <div class="alert alert-danger" v-if="error">{{error}}</div>
+       <form class="d-flex flex-column" @submit.prevent="signIn">
+         <div class="form-group">
+           <Label for="username" label="Username"/>
+           <Input type="text" id="username" name="username" v-model="username"/>
+         </div>
+         <div class="form-group">
+           <Label for="password" label="Password"/>
+           <Input type="password" id="password" name="password" v-model="password"/>
+         </div>
+         <Button type="submit" class="btn-primary ml-auto mr-auto">Sign In</Button>
+       </form>
       </CardBody>
     </Card>
   </div>
@@ -55,11 +55,8 @@ export default {
       })
         .then(response => {
           this.error = null
-          const user = JSON.stringify(response.data.user)
-          this.$store.dispatch('user', user)
+          this.$store.dispatch('user', response.data.user)
           this.$store.dispatch('jwt', response.data.jwt)
-          //localStorage.setItem('user', user)
-          //localStorage.setItem('jwt', response.data.jwt)
           this.$router.push('dashboard')
         })
         .catch((error) => {
