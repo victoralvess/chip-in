cf57dbad3c7ae0541c2dd20c7873a30be709c400
Victor Alves <vsilva.programador@gmail.com>
2018-05-10 07:56:43 -0300

    Refactor utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,19 @@
 import store from '../store'
+import decode from 'jwt-decode'
+
+export function verifyLoggedIn() { 
+  const jwt = store.getters.jwt
+  try {
+    const decoded = decode(jwt)
+    const now = Date.now() / 1000
+    return (decoded.exp < now) ? false : true
+  } catch (e) {
+    return false
+  }
+}
 
 export function isLoggedIn (to, from, next) {
-  const jwt = store.getters.jwt
-  if (jwt) {
+  if (verifyLoggedIn()) {
     next()
   } else {
     next('/')
