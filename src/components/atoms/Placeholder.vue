ae45888255ab7425ee369bd0ef0961284c34f0a8
Victor Alves <vsilva.programador@gmail.com>
2018-05-14 09:54:44 -0300

    Add Placeholder

---
+++ src/components/atoms/Placeholder.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-for="n in times">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.last {
+  margin-bottom: 20px !important;
+}
+
+@keyframes pulse {
+    0% {
+        background-color: rgba(165, 165, 165, 0.1);
+    }
+    50% {
+        background-color: rgba(165, 165, 165, 0.3);
+    }
+    100% {
+        background-color: rgba(165, 165, 165, 0.1);
+    }
+}
+</style>
+
+<script>
+
+export default {
+  props: {
+    times: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
