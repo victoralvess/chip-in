31cd2b4b9bb34839b143978e3d7b185f59351f0f
Victor Alves <vsilva.programador@gmail.com>
2018-05-15 09:28:51 -0300

    Export mongoose connection

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO);
+mongoose.connect(process.env.MONGO, {
+  autoReconnect: true,
+  connectTimeoutMS: 15000
+});
+
+module.exports = mongoose;
