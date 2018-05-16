1ca6f14820ac57a0e9f84d1639a6bc5999588def
Victor Alves <vsilva.programador@gmail.com>
2018-05-16 18:55:28 -0300

    Verify array size before show the list

--- src/components/compounds/GoalsList.vue
+++ src/components/compounds/GoalsList.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <LinePlaceholder v-if="!goals" :times="placeholders" />
-      <ListGroup v-if="goals">
-        <ListGroupItem v-for="goal of goals" :key="goal.id">
-          <div>
-           <router-link :to="{ name: 'goal', params: { id: goal.id } }">{{goal.title}}</router-link>
-           <ProgressBar :value="goal.progress"/>
-          </div>
-        </ListGroupItem>
-      </ListGroup>
+    <ListGroup v-if="goals && goals.length > 0">
+      <ListGroupItem v-for="goal of goals" :key="goal.id">
+        <div>
+          <router-link :to="{ name: 'goal', params: { id: goal.id } }">{{goal.title}}</router-link>
+          <ProgressBar :value="goal.progress"/>
+        </div>
+      </ListGroupItem>
+    </ListGroup>
   </div>
 </template>
 
