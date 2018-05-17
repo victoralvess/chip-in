2ff73397dc6a31fe5f8228ae410c67d4076c7d14
Victor Alves <vsilva.programador@gmail.com>
2018-05-17 11:26:46 -0300

    Add Form

---
+++ src/components/atoms/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <form method="POST" @submit.prevent="submitHandler">
+    <slot></slot>
+  </form>
+</template>
+
+<style lang="css" scoped>
+@import "../styles/css/form.css";
+</style>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    submitHandler: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
