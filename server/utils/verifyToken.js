3fc90eb7c3ab7d061384b945e98ae564ae3f67ae
Victor Alves <vsilva.programador@gmail.com>
2018-05-17 06:39:04 -0300

    Minor updates

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -6,13 +6,13 @@ module.exports = (req, res, next) => {
     const token = auth[1];
 
     if (auth[0] !== 'Bearer') return res.status(401).json([{ message: 'Token Error.' }]);
+
     jwt.verify(token, process.env.JWT_SECRET, function(error, decoded) {
-      if (error) res.status(401).json([{ message: 'Token Error.' }]);
+      if (error) return res.status(401).json([{ message: 'Token Error.' }]);
       req.user_jwt = decoded;
       next();
     });
   } catch (e) {
-    console.log(e)
-    res.status(401).json([{ message: 'Token Error.' }]);
+    return res.status(401).json([{ message: 'Token Error.' }]);
   }
 };
