84ee6deafae5819f3cf136c079aa705553f6b92b
Victor Alves <vsilva.programador@gmail.com>
2018-05-17 13:21:55 -0300

    Select bootstrap files

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,23 @@
 </template>
 
 <style lang="scss">
-@import '../node_modules/bootstrap/scss/bootstrap.scss';
+@import "../node_modules/bootstrap/scss/bootstrap-reboot";
+//@import "../node_modules/bootstrap/scss/bootstrap-grid";
+@import "../node_modules/bootstrap/scss/mixins/lists";
+@import "../node_modules/bootstrap/scss/mixins/transition";
+//@import "../node_modules/bootstrap/scss/root";
+@import "../node_modules/bootstrap/scss/type";
+@import "../node_modules/bootstrap/scss/grid";
+@import "../node_modules/bootstrap/scss/transitions";
+@import "../node_modules/bootstrap/scss/jumbotron";
+@import "../node_modules/bootstrap/scss/alert";
+@import "../node_modules/bootstrap/scss/utilities/flex";
+@import "../node_modules/bootstrap/scss/utilities/display";
+@import "../node_modules/bootstrap/scss/utilities/spacing";
 
 #app {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;
-  color: #2c3e50;*/
   position: absolute;
   width: 100%;
   height: 100%;
@@ -24,7 +33,8 @@
  
 a {
   &.router-link-exact-active {
-    color: green;
+    color: #383d41;
+    font-weight: bold;
   }
 }
 
