0d1b03e547c0eb5de1d88629ff21cb2c849035d5
Victor Alves <vsilva.programador@gmail.com>
2018-05-19 12:43:16 -0300

    Update error page

--- src/views/ErrorPage/ErrorPage.vue
+++ src/views/ErrorPage/ErrorPage.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center">
-    <h1 class="code">{{error.code}}</h1>
-    <p class="message">{{error.message}}</p>
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <div class="d-flex">
+      <h1 class="code">{{error.code}}</h1>
+      <p class="message">{{error.message}}</p>
+    </div>
+    <div class="space">
+      <router-link to="/" class="button">Back to Home</router-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.space {
+  margin-top: 30px;
+}
+
 .code {
   font-weight: bold;
   font-size: 70px;
@@ -16,8 +25,21 @@
 .message {
   text-align: center;
   font-size: 16px;
-  margin-top: 10px;
+  margin-top: 30px;
   color: #777770;
+}
+
+.button {
+  background: #de2200;
+  color: white;
+  font-weight: bold;
+  padding: 10px 20px 10px 20px;
+  border-radius: 50px;
+  text-decoration: none;
+}
+
+.button:hover {
+  background: #b31b00;
 }
 </style>
 
